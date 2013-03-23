3be4956e5bd4fa11be5dcb38b822ac1408dbdcde
lvrach <leoalvra@gmail.com>
2013-03-23 16:18:46 +0000

    Loader can now load multiple types: img & audio

--- scripts/core/Loader.js
+++ scripts/core/Loader.js
@@ -1,33 +1,70 @@
 Loader = {
+	FILE_EXT: {
+		sound: /(.mp3|.ogg)$/,
+		image: /(.jpg|.jpeg|.png)/
+	},
 	images: {},
 	sounds: {},
 	objects: {},
 	
 	_loadImg: function(src) { 
 		var imageCache = Loader.images;
-		var img = new Image();
 		
+		if (imageCache[src]) {
+			return  imageCache[src];
+		}
+
+		var img = new Image();
+
 		img.onload = function() { 			
 			imageCache[src] = img;
 		};
+	
 		img.src = src;
 		return img;
 
 	},
-	preload: function(src) {
-		this._loadImg(src);
+
+	_loadSound: function(src) {
+		var soundCache = Loader.sounds;
+		
+		if (soundCache[src]) {
+			return soundCache[src];
+		}
+		
+		var audio = new Audio();
+		
+		audio.onload = function() {
+			soundCache[src] = audio;
+		};
+
+		audio.src = src;
+		return audio;
+
 	},
+
+	preload: function(src) {
+		var ext = this.FILE_EXT;
+		
+		if (ext.images.test(src)) {
+			this._loadImg(src);
+		}
+		else if (ext.sounds.test()){
+			this._loadSound(src);
+		}
+	},
+
 	load: function(src) {
 		
-		var imageCache = Loader.images;
+		var ext = this.FILE_EXT;
 		
-		if (!imageCache[src]) {
-			var img = this._loadImg(src);
+		if (ext.image.test(src)) {
+			return this._loadImg(src);
 		}
-		else {
-			var img =  imageCache[src];
+		else if (ext.sound.test(src)){
+			return this._loadSound(src);
 		}
-		return img;
+
 	}
 }
 	
