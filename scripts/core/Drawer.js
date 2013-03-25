5c4e31d69b8ff9c4542df7e7e7c38775f582fd7c
lvrach <leo.al.vra@gmail.com>
2013-03-25 15:20:16 +0000

    Drawer can now scale

--- scripts/core/Drawer.js
+++ scripts/core/Drawer.js
@@ -3,39 +3,56 @@ Drawer = {
 	canvas: null ,
 	ctx: null,
 	imgToDraw: [],
+	xScale: 1,
+	yScale: 1,
+	xScaleHalf: 0.5,
+	yScaleHalf: 0.5,
 
 	init: function (canvas) {
 		this.canvas = canvas;
 		this.ctx = canvas.getContext('2d');
 
-	},
+	},	
 
+	setScale: function (x, y) {
+		this.xScale = x;
+		this.yScale = y;
+		this.xScaleHalf = x/2;
+		this.yScaleHalf = x/2;
+	},
 	//overloaded method
 	image: function (imgSrc, canvX , canvY) {
 
 		var canvRot, canvWidth, canvHeight, imgX, imgY, imgWidth, imgHeight;
 		var img,ctx;
 
+		var xScale = this.xScale,
+			yScale = this.yScale,
+			xScaleHalf = this.xScaleHalf,
+			yScaleHalf = this.yScaleHalf;
+
 		ctx = this.ctx;
 		img = Loader.load(imgSrc);
 		
 		if( arguments.length == 3) {
-			ctx.drawImage(img, canvX, canvY);
+			ctx.drawImage(img, xScale * canvX, xScale * canvY);
 		}
 		else if( arguments.length == 4) {
 			canvRot = arguments[3];
 
 			ctx.save();
-			ctx.translate(canvX, canvY);
+			ctx.translate(xScale * canvX, yScale * canvY);
 			ctx.rotate(canvRot);
-			ctx.drawImage( img, -(img.width/2), -(img.width/2));
+			ctx.drawImage( img, -(img.width * xScaleHalf ), -(img.height * yScaleHalf));
 			ctx.restore();
 		} 
 		else if ( arguments.length == 5) {
 			canvWidth =  arguments[3];
 			canvHeight = arguments[4];
-			ctx.drawImage(img, canvX -(canvWidth/2), canvY -(canvHeight/2), 
-							canvWidth, canvHeight);
+			ctx.drawImage(img, canvX * xScale -(canvWidth * xScaleHalf), 
+								canvY * yScale -(canvHeight * yScaleHalf), 
+								canvWidth * xScale, 
+								canvHeight * yScale);
 		
 		}
 		else if ( arguments.length == 6 ) {
@@ -45,10 +62,12 @@ Drawer = {
 			canvHeight = arguments[5];
 			
 			ctx.save();
-			ctx.translate(canvX, canvY);
+			ctx.translate(canvX * xScale, canvY * yScale);
 			ctx.rotate(canvRot);
-			ctx.drawImage( img, -(canvWidth/2), -(canvWidth/2), 
-							canvWidth, canvHeight);
+			ctx.drawImage( img, -(canvWidth * xScaleHalf),
+								-(canvWidth * yScaleHalf), 
+								canvWidth  * xScale,
+								canvHeight * yScale);
 			ctx.restore();
 		}
 		else if ( arguments.length == 9 ) {
@@ -61,8 +80,7 @@ Drawer = {
 			canvWidth = arguments[7];
 			canvHeight = arguments[8];
 
-			this.ctx.drawImage(img, imgX, imgY, imgHeight, imgHeight, 
-						canvX, canvY, canvWidth, canvHeight);
+			//not implemented yet
 		}
 		else if ( arguments.lenght == 10 ) {
 			imgX = arguments[1];
