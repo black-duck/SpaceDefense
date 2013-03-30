83cda1979db618c5fd45bf2b8e1fb3790004e8d9
Dimitris Touzloudis <jtouz@outlook.com>
2013-03-30 13:13:00 +0200

    New function calcY
    Calculate the y in a polynomial expression.

--- scripts/core/Utilities.js
+++ scripts/core/Utilities.js
@@ -9,9 +9,16 @@ Geometry = {
 			return Math.PI-Math.atan(x/y);
 		}
 
-	}
+	},
 	
-	
-	
+	//Calculate the y in a polynomial expression . The array must be like this [an, an-1, an-2, ..., a3, a2, a1, a0].
+	calcY: function(arrayP, x){
+		var cal = arrayP[0];
+		for(var i = 1; i < arrayP.length; i++){
+			cal *= x;
+			cal += arrayP[i];			
+		}
+		return cal;	
+	}	
 
 }
