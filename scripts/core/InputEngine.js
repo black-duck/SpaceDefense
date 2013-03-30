fd9450e500ca460ce5099dca64f8a124ea398b0a
lvrach <leo.al.vra@gmail.com>
2013-03-31 00:31:56 +0200

    Add viewport-meta and movetouch events

--- scripts/core/InputEngine.js
+++ scripts/core/InputEngine.js
@@ -28,23 +28,25 @@ InputEngine = {
                 InputEngine.bind(65, 'look-left');
                 InputEngine.bind(68, 'look-right');
 
-                //fire key bindings
-               InputEngine.bind(81,'powerup-use');  //Q
-               InputEngine.bind(69,'fire-superWeapon');  //E
-               InputEngine.bind(32,'fire-primary');  //Space
-               InputEngine.bind(16,'fire-missile');  //Shift   Ascii code=0 key code=16
+            //fire key bindings
+            InputEngine.bind(81,'powerup-use');  //Q
+            InputEngine.bind(69,'fire-superWeapon');  //E
+            InputEngine.bind(32,'fire-primary');  //Space
+            InputEngine.bind(16,'fire-missile');  //Shift   Ascii code=0 key code=16
 
 			
-                //event listeners
-                canvas.addEventListener('mousedown',InputEngine.onMouseDown,true);
-		canvas.addEventListener('mouseout',InputEngine.onMouseUp,true);
-                canvas.addEventListener('mouseup',InputEngine.onMouseUp,true);
-                canvas.addEventListener('mousemove', InputEngine.onMouseMove,true);
-                document.addEventListener('keydown', InputEngine.onKeyDown,true);
-                document.addEventListener('keyup', InputEngine.onKeyUp,true);
-                canvas.addEventListener('touchstart',InputEngine.onMouseDown,false);
-                canvas.addEventListener('touchend',InputEngine.onMouseUp,false);
+            //event listeners
+        	canvas.addEventListener('mousedown',InputEngine.onMouseDown,true);
+			canvas.addEventListener('mouseout',InputEngine.onMouseUp,true);
+        	canvas.addEventListener('mouseup',InputEngine.onMouseUp,true);
+            canvas.addEventListener('mousemove', InputEngine.onMouseMove,true);
+            document.addEventListener('keydown', InputEngine.onKeyDown,true);
+            document.addEventListener('keyup', InputEngine.onKeyUp,true);
+        	canvas.addEventListener('touchstart',InputEngine.onMouseDown,false);
+        	canvas.addEventListener('touchend',InputEngine.onMouseUp,false);
+			canvas.addEventListener('touchmove',InputEngine.onTouchMove,false);
         },
+
 	onMouseDown:function(event){
  		var action = InputEngine.bindings[32];
 		if (event.button==2){
@@ -64,6 +66,16 @@ InputEngine = {
                 if (action) {
                       InputEngine.actions[action] = false;
                 }
+	},
+	onTouchMove: function (event) {
+
+    	//make touch Event look like mouse event.
+		var rect = canvas.getBoundingClientRect();
+		InputEngine.actions['mousemove'] = true;
+		InputEngine.mouse.x = (event.targetTouches[0].clientX-rect.left) * Scale.x;
+       	InputEngine.mouse.y = (event.targetTouches[0].clientY-rect.top) * Scale.y;	
+		event.preventDefault();
+
 	},
        
     onMouseMove: function (event) {
