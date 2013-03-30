b3cc9adccab8e5769aa052fe10b35523dcbe0d47
George Chatzisofroniou <sophron@latthi.com>
2013-03-30 21:42:25 +0200

    New version of Gameplay: Used class, removed DOM methods.

--- scripts/core/start.js
+++ scripts/core/start.js
@@ -1,18 +1,29 @@
+// Used to calculate framesInterval.
+var beforeFrame = 0;
+var afterFrame = 0;
+var framesInterval = 0;
+
 DELAY = 1000.0/60.0;
 Logger = {};
 Logger.log = console.log;
 function loop() {
-		var start = new Date();
-		GameEngine.update();
-		PhysicsEngine.update();
-    	GameEngine.draw();
 
-    	window.setTimeout(loop, DELAY - (new Date() - start)  );
+    beforeFrame = new Date();
+
+    GameEngine.update();
+    PhysicsEngine.update();
+    Gameplay.update();
+    GameEngine.draw();
+
+    afterFrame = new Date();
+    framesInterval = afterFrame - beforeFrame;
+
+    window.setTimeout(loop, DELAY - framesInterval);
 }
 
 function startGame() {
 	var canvas = document.getElementById('canvas');
-    
+
     // Do some initialization.
     InputEngine.setup(canvas);
     PhysicsEngine.init(); //PhysicsEngine must be init before GameEngine.
