d7d23dcdae6de8fd700f9b9ab981b0b62c1bdae7
George Chatzisofroniou <sophron@latthi.com>
2013-03-31 21:16:01 +0300

    NPCs now shoot.

--- scripts/Ships/SimpleShip.js
+++ scripts/Ships/SimpleShip.js
@@ -12,6 +12,15 @@ factory['SimpleShip'] = Class.extend({
 	hitpoints: 10,
 	maxHitpoints: 10,
 
+    // y axis values declaring the shooting range.
+    startfire: 320,
+    endfire: 400,
+    preclusion: 100,
+
+    _fireRate: (1000/1),
+	_fireTrigger: true,
+	_fireCool: 0,
+
 	size: {
 		x: 38,
 		y: 67
@@ -26,6 +35,8 @@ factory['SimpleShip'] = Class.extend({
 		x: 0,
 		y: 0
 	},
+
+    dirshoot: new Vec2(0,-1),
 
 	img: ['ship.png','ship2.png'],
 	_frameIter: 0,
@@ -62,15 +73,27 @@ factory['SimpleShip'] = Class.extend({
 												y:-this.size.y/2 } 
 									})
 				);
+
+        
+        var plusOrMinus = Math.random() < 0.5 ? -1 : 1; 
+        this.startfire += (Math.floor(Math.random() * (this.preclusion)) + 1) * plusOrMinus;
+        this.endfire += (Math.floor(Math.random() * (this.preclusion)) + 1) * plusOrMinus;
 	
 	},
 
 	update: function() {
 
+        var pAng = this.physBody.GetAngle();
+		
+		this.angle = pAng;
+		this.dirshoot.Set(Math.cos(pAng + Math.PI/2), Math.sin(pAng +  Math.PI/2));
+		this.dirshoot.Normalize();
+
+
         var vec = new Vec2(this.dir.x, this.dir.y);
 		vec.Normalize();
 		vec.Multiply(this.speed);
-		this.physBody.SetAngle(-Math.atan(this.dir.x/this.dir.y));
+		this.physBody.SetAngle(-Math.atan(this.dir.x/this.dir.y) / 3);
 		this.physBody.SetLinearVelocity(vec);
 		
 		if (this.physBody != null) {
@@ -89,15 +112,39 @@ factory['SimpleShip'] = Class.extend({
 												) );
 			this.kill();
 		}
+ 
+        if ((this.pos.y < this.endfire) && (this.pos.y > this.startfire)) {
 
+            if (this._fireTrigger && this._fireCool == 0) {
+			    this.__fire(); 
+		    }
+		    else if (this._fireCool >  0) {
+                this._fireCool -= 1000/60
+                if (this._fireCool < 0 ) {
+                    this._fireCool = 0;
+                }
+		    }
+        }
 	},
+
+    __fire: function() {
+
+        GameEngine.spawn( new factory['Bullet'](
+					this.pos.x + (5 + this.size.x/2) * this.dirshoot.x,
+					this.pos.y + (5 + this.size.y/2) * this.dirshoot.y,
+					this.dirshoot.x,this.dirshoot.y
+				));
+        
+		SoundManager.playSound('sounds/LaserBeam1');
+		this._fireCool = this._fireRate;
+    },   
     
 	draw: function(ctx) {
 		var rad = this.physBody.GetAngle();		
 		var frame = this.img[Math.floor(this._frameIter)] ;
 		
 		Drawer.image( frame, 
-						this.pos.x, this.pos.y, rad / 2, 
+						this.pos.x, this.pos.y, rad, 
 						this.size.x, this.size.y);
 	
 		
